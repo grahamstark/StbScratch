88de5299155c3f2ef373f96102fa91caa0e1823f
Graham Stark <graham.stark@virtual-worlds.biz>
2024-04-26 09:10:18 +0100

    1st

--- scripts/tidier_legalaid_expenses.jl
+++ scripts/tidier_legalaid_expenses.jl
@@ -1,4 +1,4 @@
-using Tidier,DataFrames,CSV,PrettyTables,Format
+using Tidier,DataFrames,CSV,PrettyTables,Format,CategoricalArrays
 using ScottishTaxBenefitModel
 using .Utils
 
@@ -54,5 +54,41 @@ tot = sum( uccount.n)
 uccount.pct = 100.0 .* uccount.n ./ tot
 
 pretty_table( outf, uccount, formatters=count_pct , backend = Val(:markdown), cell_first_line_only=true)
+
+frshh17 = CSV.File( "/mnt/data/frs/2017/tab/househol.tab") |> DataFrame
+frshh18 = CSV.File( "/mnt/data/frs/2018/tab/househol.tab") |> DataFrame
+frshh19 = CSV.File( "/mnt/data/frs/2019/tab/househol.tab") |> DataFrame
+frshh20 = CSV.File( "/mnt/data/frs/2020/tab/househol.tab") |> DataFrame
+frshh21 = CSV.File( "/mnt/data/frs/2021/tab/househol.tab") |> DataFrame
+rename!( frshh17, lowercase.(names(frshh17)))
+frshh17.year = fill( 2017, size(frshh17)[1])
+rename!( frshh18, lowercase.(names(frshh18)))
+frshh18.year = fill( 2018, size(frshh18)[1])
+rename!( frshh19, lowercase.(names(frshh19)))
+frshh19.year = fill( 2019, size(frshh19)[1])
+rename!( frshh20, lowercase.(names(frshh20)))
+frshh20.year = fill( 2020, size(frshh20)[1])
+rename!( frshh21, lowercase.(names(frshh21)))
+frshh21.year = fill( 2021, size(frshh21)[1])
+
+frshh = vcat( frshh17, frshh18, frshh19, frshh20, frshh21; cols=:intersect )
+
+frshh.xweight = Weights(frshh.gross4)./3
+
+avcosts = @chain frshh begin
+   @group_by( tentyp2, year )
+   @mutate( hhrent = max(0, hhrent ), 
+            mortpay=max(0, mortpay), 
+            mortint=max(0,mortint ))
+   @summarise( 
+      m_rent = sum( hhrent.*xweight)/sum(xweight),
+      m_mort = sum( mortint.*xweight )/sum(xweight), # Weights(gross4)), 
+      m_mortpay= sum(mortpay.*xweight)/sum(xweight)) #, Weights(gross4)))
+   @arrange( tentyp2 )
+end
+
+for c in collect(avcosts)
+   pretty_table(c)
+end
 
 close(outf)
