7b3f8cc211d803e0f99b5cb493e99f2291ea2c43
Graham Stark <graham.stark@virtual-worlds.biz>
2024-10-11 21:13:13 +0200

    param thing

--- scripts/cellthing-luxor.jl
+++ scripts/cellthing-luxor.jl
@@ -2,6 +2,8 @@
 using Format
 using Luxor
 using ArgCheck
+using Colors 
+using StatsBase
 
 const GAIN_COLOUR = "#d1e7dd"
 const LOSE_COLOUR = "#f8d7da"
@@ -12,6 +14,8 @@ const HEADER_COLOR = "#b63b5e"
 const FONT = "Azo Sans"
 const FONT_SIZE = 15
 const CELL_LABELS = ["Completely Disagree", "Mostly Disagree", "Mostly Neutral", "Mostly Agree", "Completely Agree", "TOTALS"]
+const ALL_COLOURS = distinguishable_colors(50)[3:end] # sort(collect(Colors.color_names))
+const CELL_SIZE = (200,150)
 
 function fp(x::AbstractFloat)
     if x ≈ 0
@@ -29,10 +33,30 @@ end
 
 fp(x) = x
 
-function draw_crosstab( a::Matrix; cellsize=(200,150), labels::Vector{String})
-    nr,nc = collect(size(a)) .+ 1
+function draw_number_circles( 
+    counts::Vector, colours :: Vector, total :: Number )
+    sp, colours = sorted_positives( counts, colours )
+    radai = Int.(ceil.(50 .* sqrt.(sp ./ total )))
+    n = length(radai)
+    t = Table( [50], (2 .* radai ) .+ 10 ) #fill(40,n),  )
+    # t = Table( (n, ), (100,100) )
+    for i in 1:n
+        setcolor( colours[i])
+        circle(t[1,i], radai[i], action=:fill )
+        # box(t, 1, i, action=:stroke)
+    end
+    radai
+end
 
-    table = Table((nr,nc), cellsize)
+
+function draw_crosstab( 
+    ;
+    percents::Matrix, 
+    total :: Number,
+    cell_contents::Matrix, 
+    labels::Vector{String})
+    nr, nc = collect(size(percents)) .+ 1
+    table = Table( (nr,nc), CELL_SIZE )
     setfont(FONT, FONT_SIZE )
     for r in 1:nr
         for c in 1:nc          
@@ -61,7 +85,7 @@ function draw_crosstab( a::Matrix; cellsize=(200,150), labels::Vector{String})
             ac = c - 1
             pos = table[r,c]
             s = if ar > 0 && ac > 0
-                ns = fp(a[ar,ac])
+                ns = fp(percents[ar,ac])
                 pos += Point( 0, 40 )
                 if r == nr || c == nc # row/col tots bold
                     ns = "<b>$ns</b>"
@@ -76,10 +100,16 @@ function draw_crosstab( a::Matrix; cellsize=(200,150), labels::Vector{String})
                 # pos += Point(60,0)
                 "<b>$(labels[ar])</b>"
             end 
-
             settext( s, pos; halign="center", valign="bottom", markup=true ) #table[r,c], 
-                
-                # markup=true )
+            if(r in 2:(nr-1)) && (c in 2:(nc-1))
+                pos = table[r,c] - Point( 0, 20 )
+                gsave()
+                translate(pos)
+                v = cell_contents[ar,ac][1]
+                c = cell_contents[ar,ac][2]
+                draw_number_circles(v,c,total)  
+                grestore()              
+            end
         end
     end # row
 end
@@ -103,15 +133,6 @@ function sorted_positives( v :: Vector, colours :: Vector )
     return v1[1:n], ocolours
 end
 
-function draw_number_circles( 
-    counts::Vector, colours :: Vector, total :: Number )
-    sp, colours = sorted_positives( counts, colours )
-    radai = Int.(ceil.(100 .* sp ./ total ))
-    n = size(radai)
-    
-    radai
-end
-
 function drawkey(; colours::Vector, labels :: Vector )
 @argcheck length( colours ) == length(labels)
     # setfont(FONT,12)
@@ -128,24 +149,70 @@ function drawkey(; colours::Vector, labels :: Vector )
     end
 end
 
-@svg begin
-    pos = Point( 0,-520)
-    setcolor(HEADER_COLOR)
-    setfont(FONT,FONT_SIZE*3)
-    settext( "CROSSTAB OF X by Y", pos, halign="center", valign="bottom", markup=true ) 
-    # translate(150, 150)
-    draw_crosstab(a; labels=CELL_LABELS)
-    pos = Point( 0,-430)
-    setcolor("Grey60")
-    setfont(FONT,FONT_SIZE*2)
-    settext( "After Treatment", pos; halign="center", valign="bottom", markup=true ) 
-    pos = Point(-730, 200)
-    settext( "Before Treatment", pos; angle=90 ) # halign="center", valign="bottom", markup=true ) 
-    translate( 780, 0 )
-    drawkey(; colours=["red","blue","yellow","orange","black"], labels=["red","blue","yellow","orange","black"])
-end 2000 2000 "xx.svg"
+"""
+test data WRONG TOO TIRED TO FIX
+"""
+function splitupcell( v :: Number, n :: Int )::Tuple
+    remain = v
+    vs = []
+    colours = []
+    r = 0
+    for i in 1:n
+        r = rand( 1:remain )
+        push!(colours, ALL_COLOURS[i])
+        if(remain - r ) <= 0
+            push!( vs, remain )
+            push!( vs, r)
+            push!(colours, ALL_COLOURS[i])
+            break
+        else
+            remain -= r
+            push!(vs, r )
+        end
+        println( "r = $r remain=$remain")
+    end
+    # println( vs )
+    # @assert( sum(vs) ≈ v )
+    vs, colours
+end
 
-function to_pct( a :: Matrix{Int})::Matrix
+function testdraw( a :: Matrix )
+    pct, tot = to_pct(a)
+    cellcont = testsplits(a)
+    @svg begin
+        pos = Point( 0,-520)
+        setcolor(HEADER_COLOR)
+        setfont(FONT,FONT_SIZE*3)
+        settext( "CROSSTAB OF X by Y", pos, halign="center", valign="bottom", markup=true ) 
+        # translate(150, 150)
+        draw_crosstab(
+            percents=pct,
+            total=tot,
+            cell_contents=cellcont,
+            labels=CELL_LABELS)
+        pos = Point( 0,-430)
+        setcolor("Grey60")
+        setfont(FONT,FONT_SIZE*2)
+        settext( "After Treatment", pos; halign="center", valign="bottom", markup=true ) 
+        pos = Point(-730, 200)
+        settext( "Before Treatment", pos; angle=90 ) # halign="center", valign="bottom", markup=true ) 
+        translate( 780, 0 )
+        drawkey(; colours=["red","blue","yellow","orange","black"], labels=["red","blue","yellow","orange","black"])
+    end 2000 2000 "xx.svg"
+end
+
+function testsplits( a :: Matrix, n = 5 )::Matrix
+    nrows, ncols = size(a)
+    o = fill(([],[]), nrows, ncols )
+    for r in 1:nrows
+        for c in 1:ncols
+            o[r,c] = splitupcell( a[r,c], n )
+        end
+    end
+    return o
+end
+
+function to_pct( a :: Matrix )::Tuple
 @argcheck size(a)[1] == size(a)[2] # square
     nrows,ncols = collect(size( a )) .+= 1
     o = zeros(nrows,ncols)
@@ -161,5 +228,5 @@ function to_pct( a :: Matrix{Int})::Matrix
     for i in CartesianIndices(o)
         o[i] = o[i]*100/t
     end
-    return o
+    return o, t
 end
